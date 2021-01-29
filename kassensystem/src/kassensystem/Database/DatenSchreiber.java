23fbefb721fef44f4879c8ee0ce8923b3789b6a3
Philomena Moek <moek@uni-potsdam.de>
2021-01-29 19:46:48 +0100

    Dokumentation zu DatenSchreiber erstellt

--- kassensystem/src/kassensystem/Database/DatenSchreiber.java
+++ kassensystem/src/kassensystem/Database/DatenSchreiber.java
@@ -10,50 +10,71 @@ import java.util.Formatter;
 
 import Parsing.Artikel;
 
-public class DatenSchreiber 
-{
+/**
+ * 
+ * @author Felix Schulz
+ *
+ */
+
+public class DatenSchreiber {
 	private Lager lager;
 	private String ErsteZeile;
 	private String articles;
 	private String xml;
-	private String categories;
-	
-	
-	public DatenSchreiber()
-	{
+
+	/**
+	 * Konstruktor für die Klasse. TODO
+	 */
+	public DatenSchreiber() {
 		setErsteZeile();
 		articles = "<articles></articles>";
 		xml = ErsteZeile + articles;
 	}
-	
-	public DatenSchreiber(Lager lager)
-	{
+
+	/**
+	 * überladener Konstruktor für diese Klasse.
+	 * 
+	 * @param lager
+	 */
+	public DatenSchreiber(Lager lager) {
 		this.lager = lager;
 		setErsteZeile();
 		setArticles();
 		xml = ErsteZeile + articles;
 	}
-	
-	private void setErsteZeile()
-	{
+
+	/**
+	 * Setter-Methode für this.ErsteZeile this.ErsteZeile wird der String für den
+	 * XML-Header zugewiesen
+	 */
+	private void setErsteZeile() {
 		ErsteZeile = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 	}
-	
-	private void setArticles()
-	{
+
+	/**
+	 * Setter-Methode für this.articles this.articles (String) wird jeder Artikel im
+	 * XML-Format angefügt, sodass am Ende articles eine String im XML-Format
+	 * speichert, der jeden Artikel im Lager beinhaltet.
+	 */
+	private void setArticles() {
 		String Cut = "<articles>\n";
-		
-		for (Artikel article : lager.getArtikel())
-		{
+
+		for (Artikel article : lager.getArtikel()) {
 			Cut += ArtikelXML(article);
 		}
-		
+
 		Cut += "</articles>\n";
 		articles = Cut;
 	}
-	
-	private String ArtikelXML(Artikel article)
-	{
+
+	/**
+	 * Die Methode wandelt den Datentyp Artikel in einen String im XML-Format um und
+	 * gibt diesen String zurück
+	 * 
+	 * @param Artikel Ein Artikel wird übergeben
+	 * @return String Der Artikel in XML-Form wird zurückgegeben
+	 */
+	private String ArtikelXML(Artikel article) {
 		String xmlString = "\t<article>\n";
 		xmlString += "\t\t<name>" + article.getName() + "</name>\n";
 		xmlString += "\t\t<ean>" + article.getEan() + "</ean>\n";
@@ -67,18 +88,21 @@ public class DatenSchreiber
 		xmlString += "\t</article>\n";
 		return xmlString;
 	}
-	
-	public void Schreiben()
-	{
-		try
-		{
-			BufferedWriter dbData = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(System.getProperty("user.dir") + "/src/Database/database.xml"), "UTF-8"));
+
+	/**
+	 * Der aus dem Lager erstellte String (der einen XML-Header, sowie alle Artikel
+	 * im XML-Format speichert) wird in die XML-Datei geschrieben
+	 * 
+	 */
+	public void Schreiben() {
+		try {
+			BufferedWriter dbData = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(System.getProperty("user.dir") + "/src/Database/database.xml"), "UTF-8"));
 			dbData.write(xml);
 			dbData.close();
 		}
-		
-		catch(IOException e)
-		{
+
+		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
