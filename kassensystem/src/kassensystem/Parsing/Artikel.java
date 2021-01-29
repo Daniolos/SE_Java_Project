7899691f8a528874a1e15699090c902a5a3625e1
Philomena Moek <moek@uni-potsdam.de>
2021-01-29 13:51:46 +0100

    Dokumentation der Artikelklasse hinzugefügt

--- kassensystem/src/kassensystem/Parsing/Artikel.java
+++ kassensystem/src/kassensystem/Parsing/Artikel.java
@@ -2,6 +2,12 @@ package Parsing;
 
 import java.io.*;
 
+/**
+ * 
+ * @author Philomena Moek
+ * 
+ */
+
 public class Artikel {
 	private XMLParser xmlParser;
 	private String name;
@@ -15,7 +21,11 @@ public class Artikel {
 	private String einheit;
 	private String kategorie;
 
-
+/**
+ * 
+ * @param	String	Grundpreiseinheit des Artikels
+ * @return	Boolean	Ist der übergebene String eine zulässige Grundpreiseinheit?
+ */
 	public boolean checkPreiseinheit(String preiseinheit) {
 		try {
 			
@@ -33,28 +43,22 @@ public class Artikel {
 			return false;
 		}
 	}
+	/**
+	 * 
+	 * @param	String	Name des Artikels
+	 * @return	Boolean	Ist der übergebene String ein zulässiger Name?
+	 */
 
 	public boolean checkName(String name) {
 		return 2 <= name.length() && name.length() <= 32;
 	}
+	
 
-	public boolean checkKategorie(String kategorie) {
-		return kategorie.matches("[a-zäöüßA-ZÄÖÜ]+") ? (3 <= kategorie.length() && kategorie.length() <= 32) : false;
-	}
-
-	public boolean checkEan(String ean) {
-
-		if (!ean.matches("[0-9]+")) {
-			return false;
-		}
-
-		if (ean.length() == 8 || ean.length() == 13) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
+	/**
+	 * 
+	 * @param	String	Gewicht des Artikels
+	 * @return	Boolean	Ist der übergebene String ein zulässiges Gewicht?
+	 */
 	public boolean checkGewicht(String gewicht) {
 
 		if (!gewicht.matches("[0-9]*(,|.)?[0-9]+")) {
@@ -77,6 +81,11 @@ public class Artikel {
 		return true;
 	}
 
+	/**
+	 * 
+	 * @param	String	Anzahl des Artikels (Wie oft ist er im Lager?)
+	 * @return	Boolean	Ist der übergebene String eine zulässige Artikelanzahl?
+	 */
 	public boolean checkAnzahl(String anzahl) {
 
 		if (!anzahl.matches("[0-9]+")) {
@@ -89,6 +98,11 @@ public class Artikel {
 		return (1 <= Integer.parseInt(anzahl) && Integer.parseInt(anzahl) <= 1000);
 	}
 
+	/**
+	 * 
+	 * @param	String	Stückpreis des Artikels
+	 * @return	Boolean	Ist der übergebene String ein zulässiger Stückpreis?
+	 */
 	public boolean checkPreis(String preis) {
 
 		if (!preis.matches("[0-9]*(,|.)?[0-9]+")) {
@@ -107,6 +121,11 @@ public class Artikel {
 		return true;
 	}
 
+	/**
+	 * 
+	 * @param	String	Grundpreis des Artikels
+	 * @return	Boolean	Ist der übergebene String ein zulässiger Grundpreis?
+	 */
 	public boolean checkGrundpreis(String grundpreis) {
 
 		if (!grundpreis.matches("[0-9]*(,|.)?[0-9]+")) {
@@ -125,6 +144,11 @@ public class Artikel {
 		return true;
 	}
 
+	/**
+	 * 
+	 * @param	String	Einheit in der die Menge des Artikels angegeben wird
+	 * @return	Boolean	Ist der übergebene String eine zulässige Einheit?
+	 */
 	public boolean checkEinheit(String einheit) {
 
 		for (int i = 0; i < einheiten.length; i++) {
@@ -135,6 +159,19 @@ public class Artikel {
 		return false;
 	}
 
+	/**
+	 * Konstruktor für Artikel. Übernimmt Strings aller Artikeleigenschaften 
+	 * (neun Strings) und weist sie einem Artikel zu. 
+	 * @param	String	Name des Artikels
+	 * @param	String	EAN oder PLU
+	 * @param	String	Kategorie
+	 * @param	String	Einheit
+	 * @param	String	Grundpreiseinheit
+	 * @param	String	Gewicht
+	 * @param	String	Anzahl (Wie oft ist der Artikel im Lager?)
+	 * @param	String	Preis
+	 * @param	String	Grundpreis
+	 */
 	public Artikel(String name, String ean, String kategorie, String einheit, String preiseinheit, String gewicht, String anzahl,
 			String preis, String grundpreis) {
 		setName(name);
@@ -148,6 +185,11 @@ public class Artikel {
 		setEinheit(einheit);
 	}
 
+	/**
+	 * Konstruktor für Artikel. Übernimmt einen String aller Artikeleigenschaften 
+	 * (neun Strings) in XML-Form und weist sie einem Artikel zu. 
+	 * @param	String	Der String zwischen zwei <article> Tags aus einer XML-Datei
+	 */
 	public Artikel(String xml) {
 		xmlParser = new XMLParser(xml);
 		setNameFromXML();
@@ -160,7 +202,18 @@ public class Artikel {
 		setEinheitFromXML();
 		setGrundpreisFromXML();
 	}
-
+	/**
+	 * Funktion, die alle Artikeleigenschaften neu zuweist, nachdem diese geändert wurden
+	 * @param	String	Name des Artikels
+	 * @param	String	EAN oder PLU
+	 * @param	String	Kategorie
+	 * @param	String	Einheit
+	 * @param	String	Grundpreiseinheit
+	 * @param	String	Gewicht
+	 * @param	String	Anzahl (Wie oft ist der Artikel im Lager?)
+	 * @param	String	Preis
+	 * @param	String	Grundpreis
+	 */
 	public void update(String name, String ean, String kategorie, String einheit, String preiseinheit, String gewicht,
 			String anzahl, String preis, String grundpreis) {
 		setName(name);
@@ -174,6 +227,19 @@ public class Artikel {
 		setGrundpreis(grundpreis);
 	}
 	
+	/**
+	 * Die Funktion wandelt einen Artikel in ein String-Array um.
+	 * @return	String[]	Array, dass die Artikeleigenschaften in folgender Reihenfolge enthält:
+	 * 						1. Name des Artikels
+	 * 						2. EAN/ PLU
+	 * 						3. Stückpreis
+	 * 						4. Anzahl (Wie oft ist der Artikel im Lager?)
+	 * 						5. Grundpreis
+	 * 						6. Grundpreiseinheit (€/kg, €/100g,...)
+	 * 						7. Gewicht
+	 * 						8. Einheit (kg, g, l, ml, p)
+	 * 						9. Kategorie
+	 */
 	public String[] toStringArray() {
 		String[] arr = new String[9];
 		arr[0] = this.getName();
@@ -194,38 +260,74 @@ public class Artikel {
 	 * 
 	 */
 
+	/**
+	 * Getter-Methode für die Grundpreiseinheit
+	 * @return	String	Grundpreiseinheit
+	 */
 	public String getPreiseinheit() {
 		return preiseinheit;
 	}
 	
+	/**
+	 * Getter-Methode für den Namen dieses Artikels
+	 * @return	String	Artikelnamen
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Getter-Methode für die Kategorie dieses Artikels
+	 * @return	String	Kategorie
+	 */
 	public String getKategorie() {
 		return kategorie;
 	}
 
+	/**
+	 * Getter-Methode für die Mengeneinheit dieses Artikels
+	 * @return	String	Mengeneinheit
+	 */
 	public String getEinheit() {
 		return einheit;
 	}
-
+	
+	/**
+	 * Getter-Methode für die EAN dieses Artikels
+	 * @return	String	EAN
+	 */
 	public String getEan() {
 		return ean;
 	}
-
+	
+	/**
+	 * Getter-Methode für das Gewicht dieses Artikels
+	 * @return	String	Gewicht
+	 */
 	public String getGewicht() {
 		return gewicht;
 	}
 
+	/**
+	 * Getter-Methode für die im Lager vorhandene Anzhal dieses Artikels
+	 * @return	String	Anzahl
+	 */
 	public String getAnzahl() {
 		return anzahl;
 	}
 
+	/**
+	 * Getter-Methode für den Stückpreis dieses Artikels
+	 * @return	String	Stückpreis
+	 */
 	public String getPreis() {
 		return preis;
 	}
 
+	/**
+	 * Getter-Methode für den Grundpreis dieses Artikels
+	 * @return	String	Grundpreis
+	 */
 	public String getGrundpreis() {
 		return grundpreis;
 	}
@@ -236,43 +338,98 @@ public class Artikel {
 	 * 
 	 */
 
+	/**
+	 * Setter-Methode für die Grundpreiseinheit dieses Artikels. 
+	 * Überprüft vor der Zuweisung, ob die übergebene Grundpreiseinheit
+	 * zulässig ist. Ist sie das nicht, wird diesem Artikel ein leerer String
+	 * als Grundpreiseinheit zugewiesen, sonst der übergebene.
+	 * @param	String	Grundpreiseinheit
+	 */
 	public void setPreiseinheit(String preiseinheit) {
 		this.preiseinheit = checkPreiseinheit(preiseinheit) ? preiseinheit : "";
 	}
 	
+	/**
+	 * Setter-Methode für den Namen dieses Artikels. 
+	 * Überprüft vor der Zuweisung, ob der übergebene Name
+	 * zulässig ist. Ist er das nicht, wird diesem Artikel ein leerer String
+	 * als Name zugewiesen, sonst der übergebene.
+	 * @param	String	Name
+	 */
 	public void setName(String name) {
 		this.name = checkName(name) ? name : "";
 	}
-
-	//TODO Mit KategorieListe integrieren. Überprüfen unnötig?
+	
+	/**
+	 * Setter-Methode für die Kategorie dieses Artikels. 
+	 * @param	String	Kategorie
+	 */
 	public void setKategorie(String kategorie) {
-		this.kategorie = checkKategorie(kategorie) ? kategorie : "";
+		this.kategorie = kategorie;
 	}
 
+	/**
+	 * Setter-Methode für die EAN dieses Artikels. 
+	 * Überprüft vor der Zuweisung, ob die übergebene EAN
+	 * zulässig ist. Ist sie das nicht, wird diesem Artikel ein leerer String
+	 * als EAN zugewiesen, sonst die übergebene EAN.
+	 * @param	String	EAN
+	 */
 	public void setEan(String ean) {
 		this.ean = new EanVerifizierer(ean).getformatierteEan();
 	}
 
+	/**
+	 * Setter-Methode für das Gewicht dieses Artikels. 
+	 * Überprüft vor der Zuweisung, ob das übergebene Gewicht
+	 * zulässig ist. Ist es das nicht, wird diesem Artikel ein leerer String
+	 * als Gewicht zugewiesen, sonst das übergebene.
+	 * @param	String	Gewicht
+	 */
 	public void setGewicht(String gewicht) {
 		this.gewicht = checkGewicht(gewicht) ? gewicht.replace(",", ".") : "";
 	}
 
+	/**
+	 * Setter-Methode für die im Lager vorhandene Anzahl dieses Artikels. 
+	 * Überprüft vor der Zuweisung, ob die übergebene Anzahl
+	 * zulässig ist. Ist sie das nicht, wird diesem Artikel ein leerer String
+	 * als "Anzahl" zugewiesen, sonst die übergebene.
+	 * @param	String	Anzahl (Wie oft ist dieser Artikel im Lager?)
+	 */
 	public void setAnzahl(String anzahl) {
 		this.anzahl = checkAnzahl(anzahl) ? anzahl : "";
 	}
 
+	/**
+	 * Setter-Methode für den Stückpreis dieses Artikels. 
+	 * Überprüft vor der Zuweisung, ob der übergebene Stückpreis
+	 * zulässig ist. Ist er das nicht, wird diesem Artikel ein leerer String
+	 * als Stückpreis zugewiesen, sonst der übergebene.
+	 * @param	String	Stückpreis
+	 */
 	public void setPreis(String preis) {
-		if (checkPreis(preis))
-			this.preis = preis.replace(",", ".");
-//	    this.preis = checkPreis(preis.replace(",", ".")) ? preis.replace(",", ".") : "";
+	    this.preis = checkPreis(preis.replace(",", ".")) ? preis.replace(",", ".") : "";
 	}
 
+	/**
+	 * Setter-Methode für den Grundpreis dieses Artikels. 
+	 * Überprüft vor der Zuweisung, ob der übergebene Grundpreis
+	 * zulässig ist. Ist er das nicht, wird diesem Artikel ein leerer String
+	 * als Grundpreis zugewiesen, sonst der übergebene.
+	 * @param	String	Grundpreis
+	 */
 	public void setGrundpreis(String grundpreis) {
-		if (checkGrundpreis(grundpreis))
-			this.grundpreis = grundpreis.replace(",", ".");
-//	    this.grundpreis = checkGrundpreis(grundpreis.replace(",", ".")) ? grundpreis.replace(",", ".") : "";
+	    this.grundpreis = checkGrundpreis(grundpreis.replace(",", ".")) ? grundpreis.replace(",", ".") : "";
 	}
 
+	/**
+	 * Setter-Methode für die Mengeneinheit dieses Artikels. 
+	 * Überprüft vor der Zuweisung, ob die übergebene Mengeneinheit
+	 * zulässig ist. Ist sie das nicht, wird diesem Artikel ein leerer String
+	 * als Mengeneinheit zugewiesen, sonst die übergebene.
+	 * @param	String	Mengeneinheit
+	 */
 	public void setEinheit(String einheit) {
 		this.einheit = checkEinheit(einheit) ? einheit : "";
 	}
@@ -283,38 +440,66 @@ public class Artikel {
 	 * 
 	 */
 
+	/**
+	 * Weist diesem Artikel seinen in der XML-Datei gespeicherten Namen zu.
+	 */
 	private void setNameFromXML() {
 		name = xmlParser.getChild("name");
 	}
 
+	/**
+	 * Weist diesem Artikel seine in der XML-Datei gespeicherte Kategorie zu.
+	 */
 	private void setKategorieFromXML() {
 		kategorie = xmlParser.getChild("kategorie");
 	}
 
+	/**
+	 * Weist diesem Artikel seine in der XML-Datei gespeicherte EAN zu.
+	 */
 	private void setEanFromXML() {
 		ean = xmlParser.getChild("ean");
 	}
 
+	/**
+	 * Weist diesem Artikel seine in der XML-Datei gespeicherte Grundpreiseinheit zu.
+	 */
 	private void setPreiseinheitFromXML() {
 		preiseinheit = xmlParser.getChild("preiseinheit");
 	}
 
+	/**
+	 * Weist diesem Artikel sein in der XML-Datei gespeichertes Gewicht zu.
+	 */
 	private void setGewichtFromXML() {
 		gewicht = xmlParser.getChild("gewicht");
 	}
 
+	/**
+	 * Weist diesem Artikel seine in der XML-Datei gespeicherte Anzahl 
+	 * (Wie oft ist er im Lager vorhanden?) zu.
+	 */
 	private void setAnzahlFromXML() {
 		anzahl = xmlParser.getChild("anzahl");
 	}
 
+	/**
+	 * Weist diesem Artikel seinen in der XML-Datei gespeicherten Stückpreis zu.
+	 */
 	private void setPreisFromXML() {
 		preis = xmlParser.getChild("preis");
 	}
 
+	/**
+	 * Weist diesem Artikel seine in der XML-Datei gespeicherte Mengeneinheit zu.
+	 */
 	private void setEinheitFromXML() {
 		einheit = xmlParser.getChild("einheit");
 	}
 
+	/**
+	 * Weist diesem Artikel seinen in der XML-Datei gespeicherten Grundpreis zu.
+	 */
 	private void setGrundpreisFromXML() {
 		grundpreis = xmlParser.getChild("grundpreis");
 	}
