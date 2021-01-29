80227f8aa7a25d3294b9146b9386aef6eb070527
Philomena Moek <moek@uni-potsdam.de>
2021-01-29 19:21:55 +0100

    Dokumentation zu EanVerifizierer hinzugefügt

--- kassensystem/src/kassensystem/Parsing/EanVerifizierer.java
+++ kassensystem/src/kassensystem/Parsing/EanVerifizierer.java
@@ -1,57 +1,80 @@
 package Parsing;
 
-public class EanVerifizierer 
-{ // Extra Verifizierer für EAN, da EAN an mehreren Stellen benötigt wird: Beim Laden der XML, Anlegen von Artikeln, Konfiguration der PLU
-	
+/**
+ * 
+ * @author Felix Schulz
+ *
+ */
+
+public class EanVerifizierer { // Extra Verifizierer für EAN, da EAN an mehreren Stellen benötigt wird: Beim
+								// Laden der XML, Anlegen von Artikeln, Konfiguration der PLU
+
 	private String ean;
 	private String formatierteEan;
-	
-	
-	public EanVerifizierer (String ean)
-	{
+
+	/**
+	 * Konstruktor für den EanVerifizierer
+	 * Der übergebene String wird this.ean zugewiesen. 
+	 * Dann wird er formatiert zu EAN-13 und unter this.formatierteEan gespeichert.
+	 * @param	String	bekommt String mit EAN oder PLU übergeben
+	 */
+	public EanVerifizierer(String ean) {
 		setEan(ean);
 		setformatierteEan();
 	}
-	
-	public Boolean valideEan()
-	{
-		return (ean.length() == 8 || ean.length() == 13 || ean.length() == 4 || (ean.length() == 5 && String.valueOf(ean.charAt(0)).equals("9"))) && ean.matches("[0-9]+");
+
+	/**
+	 * Überprüft, ob eine EAN oder PLU zulässig ist.
+	 * @return	Boolean	true, wenn this.ean eine valide EAN (oder PLU) ist, sonst falsch
+	 */
+	public Boolean valideEan() {
+		return (ean.length() == 8 || ean.length() == 13 || ean.length() == 4
+				|| (ean.length() == 5 && String.valueOf(ean.charAt(0)).equals("9"))) && ean.matches("[0-9]+");
 	}
-		
-	private void setEan(String ean)
-	{
+
+	/**
+	 * Setter-Funktion für this.ean
+	 * @param	String	EAN die this.ean zugewiesen werden soll
+	 */
+	private void setEan(String ean) {
 		this.ean = ean;
 	}
-	
-	private void setformatierteEan()
-	{
+
+	/**
+	 * Überprüft, ob this.ean eine gültige EAN oder PLU ist.
+	 * Wenn ja, wird sie standartisiert this.formatierteEan zugewiesen,
+	 * wenn nein, wird this.formatierteEan der leere String zugewiesen.
+	 */
+	private void setformatierteEan() {
 //		String Nullen = "00000000";
 //		String Null = "";
 //		Null = ean.length() == 4 ? "0" : "";
 //		formatierteEan = valideEan() ? ean.length() < 8 ? Nullen + Null + ean : ean : "";
-		
-		if (valideEan())
-			{
-				String left = "";
-				for (int i = 0; i < 13 - ean.length(); i++)
-				{
-					left += "0";
-				}
-				formatierteEan = left + ean;	
+
+		if (valideEan()) {
+			String left = "";
+			for (int i = 0; i < 13 - ean.length(); i++) {
+				left += "0";
 			}
-			else
-			{
-				formatierteEan = "";
-			}
+			formatierteEan = left + ean;
+		} else {
+			formatierteEan = "";
+		}
 	}
-	
-	public String getEan()
-	{
+
+	/**
+	 * Getter-Funktion für this.ean
+	 * @return	String	unformatierte, nicht geprüfte EAN
+	 */
+	public String getEan() {
 		return ean;
 	}
-	
-	public String getformatierteEan()
-	{
+
+	/**
+	 * Getter-Funktion für this.formatierteEan
+	 * @return	String	formatierte, valide EAN
+	 */
+	public String getformatierteEan() {
 		return formatierteEan;
 	}
 }
