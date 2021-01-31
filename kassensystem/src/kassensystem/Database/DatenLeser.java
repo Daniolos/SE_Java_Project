0ca0cd2a55b8e6894d346e99004bd4c6914ceba2
BookaFlock <77071499+BookaFlock@users.noreply.github.com>
2021-01-31 19:37:26 +0100

    Update DatenLeser.java

--- kassensystem/src/kassensystem/Database/DatenLeser.java
+++ kassensystem/src/kassensystem/Database/DatenLeser.java
@@ -6,20 +6,42 @@ import java.io.FileNotFoundException;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Felix Schulz
+ *
+ */
+
 public class DatenLeser 
 {
 	private String data = "";
 	
+	/**
+	 * Getter-Funktion für this.data
+	 * 
+	 * @return			this.data
+	 */
 	public String getData()
 	{
 		return data;
 	}
 	
+	/**
+	 * Konstruktor für die Klasse
+	 * Die XML-Datei wird nach this.data (als ein String) eingelesen.
+	 * Existiert noch keine Datei, wird eine erstellt. this.data hat dann
+	 * den Wert "" (leerer String)
+	 */
 	public DatenLeser()
 	{
 		Lesen();
 	}
 	
+	/**
+	 * Liest gesamte XML-Datei "database.xml" als einen einzigen String ein 
+	 * und speichert sie in this.data.
+	 * Existiert noch keine XML-Datei mit diesem Namen, wird eine erstellt.
+	 */
 	public void Lesen()
 	{
 		try
@@ -37,7 +59,6 @@ public class DatenLeser
 		catch (FileNotFoundException e)
 		{
 			new DatenSchreiber().Schreiben();
-			System.out.println("Neue Datenbank wurde erstellt");
 		}	
 	}
 }
