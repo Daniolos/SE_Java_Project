234a245bcdf8180c446fbc3d05045d10847df317
BookaFlock <77071499+BookaFlock@users.noreply.github.com>
2021-01-31 19:38:03 +0100

    Update KategorieListe.java

--- kassensystem/src/kassensystem/Database/KategorieListe.java
+++ kassensystem/src/kassensystem/Database/KategorieListe.java
@@ -1,56 +1,91 @@
 package Database;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+
 import Parsing.Artikel;
+
+/**
+ * 
+ * @author Philomena Moek
+ *
+ */
 
 public class KategorieListe extends HashSet<String> {
 
+
 	/**
+	 * Methode um zu überprüfen, ob eine Kategorie zulässig ist, also in diesem
+	 * HashSet drinsteht.
 	 * 
+	 * @param kategorie			Die Kategorie, die überprüft werden soll als String
+	 * @return 					true, wenn dieses HashSet die Kategorie enthält, sonst
+	 *         falsch.
 	 */
-	private static final long serialVersionUID = 5598320182398998496L;
-
-	public boolean isEmpty() {
-		return this.isEmpty();
-	}
-
 	public boolean checkKategorie(String kategorie) {
 		return this.contains(kategorie);
 	}
 
+	/**
+	 * Methode um eine neue Kategorie hinzuzufügen. Erfüllt sie die richtigen
+	 * Anforderungen, wird sie hinzugefügt.
+	 * 
+	 * @param kategorie			Kategorie, die hinzugefügt werden soll
+	 */
 	public void addKategorie(String kategorie) {
-		if (kategorie.matches("[a-zäöüßA-ZÄÖÜ]+") && (3 <= kategorie.length() && kategorie.length() <= 32)) {
+		if (kategorie.matches("[a-zäöüßA-ZÄÖÜ]+") && (3 <= kategorie.length() && kategorie.length() <= 32) && (!kategorie.equals(null))) {
 			this.add(kategorie);
 		}
 		return;
 	}
 
+	/**
+	 * überladene Methode, die neue Kategorie von einem übergebenen Artikel
+	 * hinzufügt.
+	 * 
+	 * @param article			Artikel, dessen Kategorie neu hinzugefügt werden soll.
+	 */
 	public void addKategorie(Artikel article) {
-		if ((article.getKategorie().matches("[a-zäöüßA-ZÄÖÜ]+")) && (3 <= article.getKategorie().length()) && (article.getKategorie().length() <= 32)) {
-			this.add(article.getKategorie());
+		if (article.getKategorie().equals(null))
+			return;
+			if ((article.getKategorie().matches("[a-zäöüßA-ZÄÖÜ]+")) && (3 <= article.getKategorie().length())
+				&& (article.getKategorie().length() <= 32)) {
+				this.add(article.getKategorie());
 		}
 		return;
 	}
 
-	// TODO Wie machen wir das jetzt?
-	// für Kategorien, die nicht vergeben sind
-	public void removeKategorie(String kategorie) {
-
+	/**
+	 * Methode, um Kategorie zu entfernen. Überprüft, ob in der übergebenen Liste
+	 * ein Artikel vorkommt, der diese Kategorie verwendet, dann passiert nichts.
+	 * Kommt die Kategorie nirgendwo vor, wird sie aus diesem HashSet gelöscht.
+	 * 
+	 * @param kategorie			Die Kategorie, die entfernt werden soll.
+	 * @param articles			Das Lager, das nach Artikeln die die Kategorie
+	 * 							enthalten durchsucht werden muss
+	 */
+	public void removeKategorie(String kategorie, LinkedList<Artikel> articles) {
+		for (Artikel a : articles) {
+			if (a.getKategorie().equals(kategorie)) {
+				return;
+			}
+			this.remove(kategorie);
+		}
 		return;
 	}
-	
+
+	/**
+	 * Methode, die das HashSet in ein String Array umwandelt
+	 * 
+	 * @return 					Array, das alle hier gespeicherten Kategorien enthält
+	 */
 	public String[] toStringArray() {
 		String[] arr = new String[this.size()];
 		int count = 0;
 		for (String s : this) {
 			arr[count] = s;
+			count++;
 		}
 		return arr;
-	}
-
-	public void printer() {
-		for (String k : this) {
-			System.out.println(k);
-		}
 	}
 }
